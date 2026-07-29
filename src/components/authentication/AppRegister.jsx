f572bc7dab6bb3e40f2cb48c1e6ba7178b2c1e3e
saravanaecom <saravanaecomweb@gmail.com>
2026-07-29 12:12:04 +0530

    Add files via upload

--- src/components/authentication/AppRegister.jsx
+++ src/components/authentication/AppRegister.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import { TextField, Button, Typography, Link, IconButton, InputAdornment } from '@mui/material';
